91b84462ec78b6e583c0312b8cf63caa93c29996
Mohamed Raafat <63686204+rafa763@users.noreply.github.com>
2021-11-20 11:58:21 +0200

    Update crack.py

--- crack.py
+++ crack.py
@@ -1,17 +1,25 @@
-#get the shadow from the user
+# Get the shadow from the user
 shadow = input("shadow:")
 
+# Initialize the counter to know the line number
 counter = 0
+
+# Open the file with the hashes
 with open("hash.txt", "r") as file:
-    #reader = file.read()
+    # Iterate over each line
     for line in file:
         counter += 1
+        # Compare the hash with the shadow
         if line.strip() == shadow:
             break
-
+# Counter to comapre with the first
 linecount = 0
+# Open the file with the passwords
 with open("password.txt", "r") as file2:
+    # Iterate over each line
     for line in file2:
         linecount += 1
+        # If the two counter are equal then we found the password
         if (linecount == counter):
+            # Print the password
             print(line)
